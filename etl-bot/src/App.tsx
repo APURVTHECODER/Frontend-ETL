b1d008642ad718a66ea9d6a00cf028341f20bfd0
Apurv Choudhary <apurvsinghchoudhary@gmail.com>
2025-04-25 13:25:06 +0530

    added stable UI for table visualize

--- etl-bot/src/App.tsx
+++ etl-bot/src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import { UploadView } from '@/features/upload/UploadView';
 import './App.css';
+import BigQueryTableViewer from './features/upload/components/BigQueryTableViewer';
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light">
       <div className="min-h-screen bg-background">
-        <UploadView />
+        {/* <UploadView /> */}
+        <BigQueryTableViewer/>
         <Toaster />
       </div>
     </ThemeProvider>
